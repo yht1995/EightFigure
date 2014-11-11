242977468f10e6722b32af0ed947767d0c73142d
yht1995 <yaoht13@mails.tsinghua.edu.cn>
2014-11-11 12:03:33 +0800

    完成state类

--- EightFigure/EightFigureState.cpp
+++ EightFigure/EightFigureState.cpp
@@ -6,7 +6,7 @@ EightFigureState::EightFigureState(void)
 {
 }
 
-EightFigureState::EightFigureState(int *data)
+EightFigureState::EightFigureState(char *data)
 {
     this->data = 0;
     for (int i = 0;i<9;i++)
@@ -19,15 +19,140 @@ EightFigureState::~EightFigureState(void)
 {
 }
 
-int* EightFigureState::GetDataArray(int *data)
+char* EightFigureState::GetDataArray(char *data)
 {
     unsigned int temp = this->data;
     for (int i = 0;i<9;i++)
     {
-        data[i] = temp % 10;
+        data[8-i] = temp % 10;
         temp = temp /10;
     }
     return data;
 }
 
+ //r   c  0   1   2   3   4
+ // 
+ //0      0   0   0   0   0
+ //1      0   0   1   2   0
+ //2      0   3   4   5   0  
+ //3      0   6   7   8   0  
+ //4      0   0   0   0   0
+bool EightFigureState::Move(Direction direction)
+{
+    char data[5][5];
+    bool result;
+    memset(data,0,25*sizeof(char));
+    char array[9];
+    GetDataArray(array);
+    int zeroc,zeror;
+    for (int r = 1;r <= 3;r++)
+    {
+        for (int c = 1;c <= 3;c++)
+        {
+            data[r][c] = array[(r-1)*3+(c-1)];
+            if (data[r][c] == 0)
+            {
+                zeroc = c;
+                zeror = r;
+            }
+        }
+    }
+    switch (direction)
+    {
+    case up:
+        if (data[zeror-1][zeroc])
+        {
+            swap(data[zeror][zeroc],data[zeror-1][zeroc]);
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+        break;
+    case down:
+        if (data[zeror+1][zeroc])
+        {
+            swap(data[zeror][zeroc],data[zeror+1][zeroc]);
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+        break;
+    case left:
+        if (data[zeror][zeroc-1])
+        {
+            swap(data[zeror][zeroc],data[zeror][zeroc-1]);
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+        break;
+    case right:
+        if (data[zeror][zeroc+1])
+        {
+            swap(data[zeror][zeroc],data[zeror][zeroc+1]);
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+        break;
+    default:
+        break;
+    }
+    for (int r = 1;r <= 3;r++)
+    {
+        for (int c = 1;c <= 3;c++)
+        {
+            array[(r-1)*3+(c-1)] = data[r][c];
+        }
+    }
+    this->data = 0;
+    for (int i = 0;i<9;i++)
+    {
+        this->data = this->data * 10 + array[i];
+    }
+    return result;
+}
 
+void EightFigureState::swap(char& a,char& b)
+{
+    char t = a;
+    a = b;
+    b = t;
+}
+
+int EightFigureState::InverseOrder()
+{
+    char array[9];
+    GetDataArray(array);
+    int inverseOrder = 0;
+    for (int i = 1;i<9;i++)
+    {
+        if (array[i] !=0)
+        {
+            for (int j =0;j<i;j++)
+            {
+                if (array[j] != 0)
+                {
+                    if (array[j] > array[i])
+                    {
+                        inverseOrder ++;
+                    }
+                }
+            }
+        }
+    }
+    return inverseOrder;
+}
+
+bool EightFigureState::CanSolve(EightFigureState c)
+{
+    return((this->InverseOrder()%2) == (c.InverseOrder()%2));
+}
--- EightFigure/EightFigureState.h
+++ EightFigure/EightFigureState.h
@@ -1,17 +1,28 @@
 #pragma once
+enum Direction
+{
+    up = 0,
+    down,
+    left,
+    right
+};
 class EightFigureState
 {
 private:
     unsigned int data;
+    void swap(char& a,char& b);
 public:
     EightFigureState(void);
-    EightFigureState(int *data);
-    int* GetDataArray(int *data);
-    bool operator == (const EightFigureState& c);
+    EightFigureState(char *data);
     ~EightFigureState(void);
+    char* GetDataArray(char *data);
+    bool Move(Direction direction);
+    int InverseOrder();
+    bool CanSolve(EightFigureState c);
+    bool operator == (const EightFigureState c);
 };
 
-inline bool EightFigureState::operator == (const EightFigureState& c)
+inline bool EightFigureState::operator == (const EightFigureState c)
 {
     return (this->data == c.data);
 }
