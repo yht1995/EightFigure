26065614a35542cd9c4700a890e05bd1add34876
yht1995 <yaoht13@mails.tsinghua.edu.cn>
2014-11-11 12:03:50 +0800

    单元测试

---
+++ UnitTest/unittest1.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../EightFigure/EightFigureState.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(GetDataArray)
+		{
+            char data[9] = {1,2,3,4,5,6,7,8,0};
+			EightFigureState state(data);
+            char result[9];
+            state.GetDataArray(result);
+            for (int i = 0;i<9;i++)
+            {
+                Assert::AreEqual(data[i],result[i]);
+            }
+		}
+        TEST_METHOD(Move)
+        {
+            char data[9] = {1,2,3,4,5,6,7,8,0};
+            char gold[9] = {1,2,3,4,5,6,7,0,8};
+            EightFigureState state(data);
+            char result[9];
+            Assert::AreEqual(state.Move(left),true);
+            state.GetDataArray(result);
+            for (int i = 0;i<9;i++)
+            {
+               Assert::AreEqual(gold[i],result[i]);
+            }
+        }
+        TEST_METHOD(InverseOrder)
+        {
+            char data[9] = {1,0,2,3,4,5,6,7,8};
+            EightFigureState state(data);
+            int result  = state.InverseOrder();
+            Assert::AreEqual(result,0);
+        }
+        TEST_METHOD(CanSlove)
+        {
+            char data1[9] = {1,2,3,4,5,6,7,8,0};
+            char data2[9] = {1,2,3,4,5,6,7,0,8};
+            EightFigureState state1(data1);
+            EightFigureState state2(data2);
+            Assert::AreEqual(state1.CanSolve(state2),true);
+        }
+	};
+}
